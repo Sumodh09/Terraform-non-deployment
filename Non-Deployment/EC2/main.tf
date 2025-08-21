ee2084d79234a6c0ba938b498af1d4caffba0010
Sumodh c Sabu <52120967+Sumodh09@users.noreply.github.com>
2025-08-21 11:06:25 +0530

    Update main.tf

--- Non-Deployment/EC2/main.tf
+++ Non-Deployment/EC2/main.tf
@@ -1,6 +1,13 @@
 # Get data for all EC2 instances in the region
 data "aws_instances" "all" {}
 
+resource "aws_s3_bucket" "my_bucket" {
+  bucket = "demo-csv-separate-bucket-name" 
+#   lifecycle {
+#    prevent_destroy = true
+#  }
+}
+
 # Loop through instance IDs to get detailed info
 data "aws_instance" "details" {
   for_each = toset(data.aws_instances.all.ids)
@@ -16,4 +23,25 @@ output "ec2_instance_details_json" {
       tags              = instance.tags
     }
   })
+}
+
+resource "null_resource" "apply" {
+  provisioner "local-exec" {
+    command = "terraform apply -auto-approve" 
+  }
+}
+
+resource "null_resource" "json_upload" {
+  provisioner "local-exec" {
+    command = "terraform output -raw ec2_instance_details_json > ec2_data.json" 
+  }
+}
+
+resource "aws_s3_object" "my_script_zip" {
+  bucket = aws_s3_bucket.my_bucket.bucket
+  key    = "ec2_data.json" 
+  source = "${path.module}/EC2/ec2_data.json"
+  acl    = "private"
+
+  depends_on = [null_resource.json_upload]
 }
