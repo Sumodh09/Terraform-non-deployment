9565ef6091873c75b4a276f944311d4bbe2b5c35
Sumodh c Sabu <52120967+Sumodh09@users.noreply.github.com>
2025-11-05 14:05:58 +0530

    Update main.tf

--- Non-Deployment/EC2/main.tf
+++ Non-Deployment/EC2/main.tf
@@ -37,6 +37,34 @@ resource "aws_s3_object" "ec2_detail_object" {
   source = local_file.ec2_detail_file.filename
   #etag   = filemd5(local_file.ec2_detail_file.filename)
 }
-output "ec2_instance_details_json" {
-  value = local.ec2_details_json
+#output "ec2_instance_details_json" {
+ # value = local.ec2_details_json
+#}
+
+locals {
+  ec2_compliance_csv = join("\n", concat(
+    ["Instance_ID,Availability_Zone,Compliance_Status,Reason"],
+    [
+      for id, inst in data.aws_instance.details :
+      format(
+        "%s,%s,%s,%s",
+        id,
+        inst.availability_zone,
+        (startswith(inst.availability_zone, "us-east-1") ? "Compliant" : "Non-Compliant"),
+        (startswith(inst.availability_zone, "us-east-1") ? "Availability zone is us-east-1" : "Availability zone is not us-east-1")
+      )
+    ]
+  ))
+}
+
+resource "local_file" "ec2_compliance_csv" {
+  content  = local.ec2_compliance_csv
+  filename = "${path.module}/ec2_compliance_report.csv"
+}
+
+resource "aws_s3_object" "ec2_compliance_object" {
+  bucket = aws_s3_bucket.my_bucket.bucket
+  key    = "Compliance_Report/EC2/ec2_compliance_report.csv"
+  source = local_file.ec2_compliance_csv.filename
+  etag   = filemd5(local_file.ec2_compliance_csv.filename)
 }
